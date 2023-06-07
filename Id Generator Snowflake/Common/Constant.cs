b0c348b83e794196182ff35cfc3450670c101191
Yami An <yamiannephilim@gmail.com>
2023-06-07 23:41:47 +0700

    20230607
    Co-authored-by: nguyenminhdat <longkunz@gmail.com>

---
+++ Id Generator Snowflake/Common/Constant.cs
@@ -0,0 +1,14 @@
+﻿namespace Id_Generator_Snowflake.Common;
+
+internal static class Constant
+{
+    internal const int WKR_ID_BITS = 3; // Số bit được sử dụng để lưu trữ Worker Id
+    internal const int DC_ID_BITS = 2;  // Số bit được sử dụng để lưu trữ Datacenter Id
+    internal const int SEQ_BITS = 8;    // Số bit được sử dụng để lưu trữ Sequence
+
+    internal const ulong MAX_WKR_ID = (1 << WKR_ID_BITS) - 1;   // Giá trị tối đa của Worker Id
+    internal const ulong MAX_DC_ID = (1 << DC_ID_BITS) - 1;     // Giá trị tối đa của Datacenter Id
+    internal const ulong SEQ_MASK = (1 << SEQ_BITS) - 1;        // Mặt nạ để lấy các bit của Sequence
+    internal const int WKR_ID_SHFT = SEQ_BITS;                  // Số bit cần dịch trái để lưu trữ Worker Id
+    internal const int DC_ID_SHFT = SEQ_BITS + WKR_ID_BITS;     // Số bit cần dịch trái để lưu trữ Datacenter Id
+}
---
+++ Id Generator Snowflake/Utilities/Util.cs
@@ -0,0 +1,26 @@
+﻿using static System.DateTimeOffset;
+
+namespace Id_Generator_Snowflake.Utilities;
+
+internal static class Util
+{
+    /// <summary>
+    /// Lấy thời điểm timestamp hiện tại.
+    /// </summary>
+    internal static ulong TimeGen() => (ulong)UtcNow.ToUnixTimeMilliseconds();
+
+    /// <summary>
+    /// Tìm thời điểm timestamp kế tiếp lớn hơn hoặc bằng thời điểm cuối cùng.
+    /// </summary>
+    internal static ulong TilNextMillis(this ulong lastTimestamp)
+    {
+        var timestamp = TimeGen();
+
+        while (timestamp <= lastTimestamp)
+        {
+            timestamp = TimeGen();
+        }
+
+        return timestamp;
+    }
+}
